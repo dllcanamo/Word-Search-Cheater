312ee0343b60b77feb8ce0b68e1933fcdc428b49
levi <dllcanamo@gmail.com>
2023-01-19 10:38:23 +0800

    finished puzzle setup, fixed minor bug on container class

--- classes/container.py
+++ classes/container.py
@@ -1,8 +1,8 @@
 class container:
     def __init__(self, value, position):
-        if isinstance(value, str):
-            raise TypeError
-        if isinstance(position, tuple):
-            raise TypeError
+        if not isinstance(value, str):
+            raise TypeError('value should be a string only')
+        if not isinstance(position, tuple):
+            raise TypeError('value should be a tuple only')
         self.value = value
         self.position = position
--- puzzle_setup.py
+++ puzzle_setup.py
@@ -1,15 +1,19 @@
 import classes.container as cont
 import file_reader as fr
 
-# _cont = cont.container(1,2)
-# print(_cont.value)
-
+# def setup_puzzle():
 puzzle_arr = fr.get_puzzle_or_count('puzzle')
 for line in puzzle_arr:
     print(line)
 puzzle_container_list = []
-for r_index,row in enumerate(puzzle_arr):
+for r_index, row in enumerate(puzzle_arr):
     row_container = []
+    for col_index, line in enumerate(row):
+        row_container.append(cont.container(line, (r_index, col_index)))
+    puzzle_container_list.append(row_container)
+# for line in puzzle_container_list:
+#     print([item.position for item in line])
+
 
     
 
